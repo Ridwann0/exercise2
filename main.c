503ae6cd2754bcf5449cb94f2e16a1ff019fa1a2
Ridwan Mohamed <Ridwann04@hotmail.com>
2023-09-26 11:06:25 +0200

    print from 2 to 1

--- main.c
+++ main.c
@@ -2,5 +2,15 @@
 
 int main(void) 
 {
+    for (int i = 1; i < 4; i++)
+    {
+    printf("%d ", i);   
+    }
+
+    for (int i = 2; i > 0; i--)
+    {
+    printf("%d  ", i);   
+    }
+    printf("\n");
 	return 0;
 }
